3c9b599d79ac422c062926e3b7efc31946a884f9
MaXimeHH <maxime.debruyne1@epsi.fr>
2024-02-10 10:26:46 +0100

    init project

---
+++ src/main/java/com/back/payetonkawa/PayetonkawaApplication.java
@@ -0,0 +1,13 @@
+package com.back.payetonkawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayetonkawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayetonkawaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back/payetonkawa/PayetonkawaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.payetonkawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayetonkawaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
